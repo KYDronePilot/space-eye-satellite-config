63c46d309ef9ee5926749006fb8bbbae592b3bb7
KYDronePilot <kydronepilotdev@gmail.com>
2020-07-27 00:10:11 -0400

    Spelling error

--- gulpfile.js
+++ gulpfile.js
@@ -5,5 +5,5 @@ gulp.task('build-yaml', () => {
     return gulp
         .src('./config.yaml')
         .pipe(yaml({ space: 2 }))
-        .pipe(gulp.dest('./dest/'))
+        .pipe(gulp.dest('./dist/'))
 })
